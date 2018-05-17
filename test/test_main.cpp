2142a1fcef529f9189cde50c0a1ee7708e01c2ae
jasper <Jasper.Smienk@student.hu.nl>
2018-05-17 15:07:14 +0200

    Added test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,22 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "../src/time_manager.hpp"
+#include "../src/rtc_time.hpp"
+#include "../src/wrap-hwlib.hpp"
 
 TEST_CASE( "Example Test Case" ) {
     REQUIRE(10 == 10);
+}
+
+TEST_CASE("Time is running") {
+    auto scl = hwlib::target::pin_oc(hwlib::target::pins::scl);
+    auto sda = hwlib::target::pin_oc(hwlib::target::pins::sda);
+
+    auto clock = time::TimeManager(scl, sda);
+
+    auto firstTime = clock.getTime().getSeconds();
+    hwlib::wait_ms(2000);
+    auto secondTime = clock.getTime().getSeconds();
+
+    REQUIRE(firstTime != secondTime);
 }
