dd35f8256f2bc5129c3d897f9401024b8c59f35a
Draivun <nick_goris@outlook.com>
2018-06-19 12:36:26 +0200

    Implemented ENUM class, started on Switch statements to implement STD

--- src/enumStates.hpp
+++ src/enumStates.hpp
@@ -1,7 +1,13 @@
-enum timeManagerStates {
+#ifndef ENUMSTATES_HPP
+#define ENUMSTATES_HPP
+
+enum class timeManagerStates {
+    MAIN_MENU,    // Main Menu
     TIMER_SELECT, // Main menu select Timer
     ALARM_SELECT, // Main menu select Alarm
     TIMER,        // Timer menu state
     ALARM,        // Alarm menu state
     SET_ALARM     // Setting an alarm
-}
+};
+
+#endif // ENUMSTATES_HPP
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 #include "wrap-hwlib.hpp"
 
-#include "enumSTates.hpp"
+#include "enumStates.hpp"
 #include "rtc_time.hpp"
 #include "time_manager.hpp"
 
@@ -32,37 +32,107 @@ int main() {
     enum timeManagerStates timeManagerState;
 
     char someInput;
+
     while (true) {
-        hwlib::cin >> someInput;
-        if (someInput == 'q') {
-            clock.setTimer(timerCounter);
-        }
-        if (someInput == 'w') {
-            clock.resetTimer(timerCounter);
-        }
-        if (someInput == 'e') {
-            clock.clearTimer(timerCounter);
-        }
+        auto now = clock.getTime();
+        timeManagerState = timeManagerStates::MAIN_MENU;
 
-        if (someInput == 'o') {
-            if (timerCounter > 0) {
-                timerCounter--;
+        switch (timeManagerState) {
+
+        case timeManagerStates::MAIN_MENU:
+            hwlib::cout << clock.getTime().getTotalSeconds() << hwlib::endl;
+            hwlib::cout << "Press 1 to cycle through menu, press 2 to select menu, press 3 to cancel" << hwlib::endl;
+
+            if (clock.getTime() - now >= 60) {
+                hwlib::cout << clock.getTime().getTotalSeconds() << hwlib::endl;
+                now = clock.getTime();
             }
-            hwlib::cout << timerCounter + 1 << hwlib::endl;
-        }
-        if (someInput == 'p') {
-            if (timerCounter < 4) {
-                timerCounter++;
+
+            hwlib::cin >> someInput;
+
+            if (someInput == '1') {
+                timeManagerState = timeManagerStates::TIMER_SELECT;
             }
-            hwlib::cout << timerCounter + 1 << hwlib::endl;
-        }
+            break;
 
-        hwlib::cout << "Current second count is: \t" << clock.getTime().getTotalSeconds() << hwlib::endl;
+        case timeManagerStates::TIMER_SELECT:
+            hwlib::cout << "Timer menu? Press 2 to confirm, press 3 to cancel" << hwlib::endl;
+            hwlib::cin >> someInput;
+            if (someInput == '1') {
+                timeManagerState = timeManagerStates::ALARM_SELECT;
+            }
+            if (someInput == '2') {
+                timeManagerState = timeManagerStates::TIMER;
+            }
+            if (someInput == '3') {
+                timeManagerState = timeManagerStates::MAIN_MENU;
+            }
+            break;
 
-        for (uint16_t i = 0; i < timersSize; i++) {
-            hwlib::cout << (clock.elapsedTime(i).getTotalSeconds()) << '\t';
+        case timeManagerStates::ALARM_SELECT:
+            hwlib::cout << "Alarm menu? Press 2 to confirm, press 3 to cancel" << hwlib::endl;
+            hwlib::cin >> someInput;
+            if (someInput == '1') {
+                timeManagerState = timeManagerStates::TIMER_SELECT;
+            }
+            if (someInput == '2') {
+                timeManagerState = timeManagerStates::ALARM;
+            }
+            if (someInput == '3') {
+                timeManagerState = timeManagerStates::MAIN_MENU;
+            }
+            break;
+
+        case timeManagerStates::TIMER:
+            hwlib::cout << "Timer selected. Press 3 to return to main menu" << hwlib::endl;
+            hwlib::cin >> someInput;
+            if (someInput == '3') {
+                timeManagerState = timeManagerStates::MAIN_MENU;
+            }
+            break;
+
+        case timeManagerStates::ALARM:
+            hwlib::cout << "Alarm selected. Press 3 to return to main menu" << hwlib::endl;
+            hwlib::cin >> someInput;
+            if (someInput == '3') {
+                timeManagerState = timeManagerStates::MAIN_MENU;
+            }
+            break;
         }
-        hwlib::cout << hwlib::endl;
     }
+
+    // char someInput;
+    // while (true) {
+    //     hwlib::cin >> someInput;
+    //     if (someInput == 'q') {
+    //         clock.setTimer(timerCounter);
+    //     }
+    //     if (someInput == 'w') {
+    //         clock.resetTimer(timerCounter);
+    //     }
+    //     if (someInput == 'e') {
+    //         clock.clearTimer(timerCounter);
+    //     }
+
+    //     if (someInput == 'o') {
+    //         if (timerCounter > 0) {
+    //             timerCounter--;
+    //         }
+    //         hwlib::cout << timerCounter + 1 << hwlib::endl;
+    //     }
+    //     if (someInput == 'p') {
+    //         if (timerCounter < 4) {
+    //             timerCounter++;
+    //         }
+    //         hwlib::cout << timerCounter + 1 << hwlib::endl;
+    //     }
+
+    //     hwlib::cout << "Current second count is: \t" << clock.getTime().getTotalSeconds() << hwlib::endl;
+
+    //     for (uint16_t i = 0; i < timersSize; i++) {
+    //         hwlib::cout << (clock.elapsedTime(i).getTotalSeconds()) << '\t';
+    //     }
+    //     hwlib::cout << hwlib::endl;
+    // }
     return 0;
 }
